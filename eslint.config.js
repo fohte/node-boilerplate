7d64e5ddfdb2e899a516dbbd1baae84af430ea6f
Hayato Kawai <fohte.hk@gmail.com>
2025-06-14 02:04:36 +0900

    remove explicit ignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,5 @@
 import { mainConfig, typescriptConfig } from '@fohte/eslint-config'
 
-const config = [
-  ...mainConfig,
-  ...typescriptConfig,
-  {
-    ignores: ['node_modules/', 'dist/', 'coverage/', '*.log'],
-  },
-]
+const config = [...mainConfig, ...typescriptConfig]
 
 export default config
